7b8202a2c6695ff413de9e3dd396637e060766e4
王振福 <wangzhenfu@zto.ldap>
2018-11-22 18:19:31 +0800

    update css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import { Icon } from 'semantic-ui-react';
 
-// import 'semantic-ui-css/semantic.min.css';
 import '@commonScss/semantic.scss';
 import '@commonScss/index.scss';
 
@@ -10,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Icon className="twitter" />
         <p>Welcome to News!</p>
         <p>在 .flowconfig options 中添加 在 js 文件中报错行添加 完美解决!!</p>
         <Button color="purple">Purple</Button>
